2d03a00ff9fb75f00412d2ec8ff6ce97c572e081
Udit-Naik <uditnaik2204@gmail.com>
2024-06-08 18:56:45 +0530

    add javaScript

---
+++ Calc.js
@@ -0,0 +1,18 @@
+function Solve(val) {
+    var v = document.getElementById('res');
+    v.value += val;
+
+ }
+const Result=()=> {
+    let num1 = document.getElementById('res').value;
+    let num2 = eval(num1);
+    document.getElementById('res').value = num2;
+ }
+ const Clear=() =>{
+    let inp = document.getElementById('res');
+    inp.value = '';
+ }
+ const Back=()=> {
+    let  ev = document.getElementById('res');
+    ev.value = ev.value.slice(0,-1);
+ }  
